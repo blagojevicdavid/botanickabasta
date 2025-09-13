8325707f3d12dd3456ce64ccd5cd997745e3449e
David <davidblagojevic7@gmail.com>
2025-09-13 23:30:57 +0200

    Ubacen datagrid sa ispisom i placeholder za mapu, napravljene osnovne klase

---
+++ BotanickaBasta/Bastovan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotanickaBasta
+{
+    public class Bastovan
+    {
+        private string ime;
+        private string prezime;
+        private int id;
+        private string telefon;
+        private List<Biljka> zaduzeneBiljke;
+
+        public Bastovan (string ime, string prezime, int id, string telefon)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.id = id;
+            this.telefon = telefon;
+            this.zaduzeneBiljke = new List<Biljka> ();
+        }
+
+        public string Ime { get { return ime; } set { ime = value; } }
+        public string Prezime { get { return prezime; } set { prezime = value; } }
+        public int Id { get { return id; } set { id = value; } }    
+        public string Telefon { get { return telefon; } set { telefon = value; } }
+        public List<Biljka> ZaduzeneBiljke { get { return zaduzeneBiljke; }  set { zaduzeneBiljke = value; } }
+
+
+
+
+    }
+}
--- BotanickaBasta/MainWindow.xaml.cs
+++ BotanickaBasta/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,29 @@ namespace BotanickaBasta
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<Biljka> biljka;
+        private List<Bastovan> bastovan;
+
+        public ObservableCollection<Bastovan> Bastovani { get; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;   // da {Binding Bastovani} “vidi” kolekciju
+
+            // test podaci
+            var g1 = new Bastovan("Marko", "Petrović", 1, "0601234567");
+            var g2 = new Bastovan("Jelena", "Jovanović", 2, "0619876543");
+
+            g1.ZaduzeneBiljke.Add(new Biljka("Ruža"));
+            g2.ZaduzeneBiljke.Add(new Biljka("Lala"));
+            g2.ZaduzeneBiljke.Add(new Biljka("Zumbul"));
+
+            Bastovani.Add(g1);
+            Bastovani.Add(g2);
+
         }
+
     }
 }
